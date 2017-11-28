d9aacc0e89107b636f82111cd0e6ae641eab59d9
Anders Bootsmann <ande4559@edu.eal.dk>
2017-11-28 20:11:15 +0100

    First commit
    tilføjet datatypes, templates, smule kode på frontpage, kodet menu

---
+++ codegarden18/scripts/functions.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function ($) {
+
+    $(document).ready(function () {
+        $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+            $(this).toggleClass('open');
+            $(".nav").toggleClass('open');
+        });
+    });
+
+
+});
+                
